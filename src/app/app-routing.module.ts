35caf291517f8bdabfaccf2fcc392d533ef54580
alcaudon71 <alcaudon71@gmail.com>
2023-09-29 09:13:33 +0200

    sidenav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { DashboardRoutingModule } from "./modules/dashboard/dashboard-routing.module";
 
 const routes: Routes=[
     { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
@@ -9,7 +10,9 @@ const routes: Routes=[
     imports: [RouterModule.forRoot(
         routes,
         {enableTracing: false, useHash: true}
-    )],
+    ),
+    DashboardRoutingModule
+    ],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -0,0 +1,27 @@
+import { MediaMatcher } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.css']
+})
+export class SidenavComponent implements OnInit {
+
+  mobileQuery: MediaQueryList;  // obtener Medios disponibles para el dispositivo
+
+  menuNav = [
+    {name: "Home", route: "home", icon: "home"},
+    {name: "Categorias", route: "categories", icon: "category"},
+    {name: "Productos", route: "products", icon: "production_quantity_limits"}
+  ];
+
+  constructor(media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)'); // Establece un maximo de 600px para considerar el Medio como android
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
